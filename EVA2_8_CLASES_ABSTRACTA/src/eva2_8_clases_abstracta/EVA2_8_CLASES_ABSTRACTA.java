292c421851e4b4b9d22380694497e7d05e83bb2e
KevinChicoD <143564440+KevinChicoD@users.noreply.github.com>
2024-03-27 14:08:13 -0600

    Add files via upload

---
+++ EVA2_8_CLASES_ABSTRACTA/src/eva2_8_clases_abstracta/EVA2_8_CLASES_ABSTRACTA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_clases_abstracta;
+
+/**
+ *
+ * @author KEVIN
+ */
+public class EVA2_8_CLASES_ABSTRACTA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Estudiante estu = new Estudiante("24871", "Kevin", "Chico", 18);
+        estu.impirmirDatos();
+    }
+    
+}
